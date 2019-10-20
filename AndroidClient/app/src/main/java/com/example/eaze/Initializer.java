05b996c225939c0f5eab7acbd484c19630098b38
Christos Falas <raspberrymaker@gmail.com>
2019-10-20 15:27:51 +0300

    added android code

---
+++ AndroidClient/app/src/main/java/com/example/eaze/Initializer.java
@@ -0,0 +1,17 @@
+package com.example.eaze;
+
+import android.app.Application;
+
+import net.gotev.uploadservice.UploadService;
+
+public class Initializer extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        // setup the broadcast action namespace string which will
+        // be used to notify upload status.
+        // Gradle automatically generates proper variable as below.
+        UploadService.NAMESPACE = BuildConfig.APPLICATION_ID;
+    }
+}
